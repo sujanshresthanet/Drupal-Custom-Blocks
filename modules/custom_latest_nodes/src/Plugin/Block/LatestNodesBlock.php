438a0bd14301a63b599b8d2a4f784e5c53201225
Sujan Shrestha <40193-sujan.shrestha@users.noreply.drupalcode.org>
2024-04-08 09:16:14 +0545

    Block Category added

--- modules/custom_latest_nodes/src/Plugin/Block/LatestNodesBlock.php
+++ modules/custom_latest_nodes/src/Plugin/Block/LatestNodesBlock.php
@@ -13,6 +13,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @Block(
  *   id = "custom_latest_nodes_block",
  *   admin_label = @Translation("Latest Nodes"),
+ *   category = @Translation("Custom Blocks")
  * )
  */
 class LatestNodesBlock extends BlockBase implements ContainerFactoryPluginInterface {
