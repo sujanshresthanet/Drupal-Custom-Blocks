b60c74ea43520d693dec364fff05c850964f9883
Sujan Shrestha <sujanshrestha.net@gmail.com>
2024-04-08 11:33:07 +0545

    Comments added
    Co-authored-by: Drupal Dev Master <sujan.shrestha.k@gmail.com>

--- modules/custom_taxonomy_block/src/Plugin/Block/CustomTaxonomyBlock.php
+++ modules/custom_taxonomy_block/src/Plugin/Block/CustomTaxonomyBlock.php
@@ -20,19 +20,23 @@ class CustomTaxonomyBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    // Retrieve block configuration.
     $config = $this->getConfiguration();
+    // Retrieve vocabulary id from block configuration, default to empty string if not set.
     $vid = isset($config['vid']) ? $config['vid'] : '';
 
     $terms = [];
+    // If vocabulary id is not empty, load the terms associated with it.
     if (!empty($vid)) {
       $terms = \Drupal::entityTypeManager()
         ->getStorage('taxonomy_term')
         ->loadTree($vid);
     }
 
+    // Return render array defining the block content.
     return [
-      '#theme' => 'custom_taxonomy_block_template',
-      '#terms' => $terms,
+      '#theme' => 'custom_taxonomy_block_template', // Theme hook for the block content.
+      '#terms' => $terms, // Pass loaded terms to the template.
     ];
   }
 
@@ -40,22 +44,27 @@ class CustomTaxonomyBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
+    // Call parent blockForm method to build the basic block configuration form.
     $form = parent::blockForm($form, $form_state);
 
+    // Retrieve block configuration.
     $config = $this->getConfiguration();
 
+    // Load all vocabularies.
     $vocabularies = \Drupal\taxonomy\Entity\Vocabulary::loadMultiple();
+    // Prepare vocabulary options for select list.
     $options = [];
     foreach ($vocabularies as $vid => $vocabulary) {
       $options[$vid] = $vocabulary->label();
     }
 
+    // Add vocabulary select field to the block configuration form.
     $form['vid'] = [
       '#type' => 'select',
       '#title' => $this->t('Select Vocabulary'),
-      '#default_value' => isset($config['vid']) ? $config['vid'] : '',
-      '#options' => $options,
-      '#required' => TRUE,
+      '#default_value' => isset($config['vid']) ? $config['vid'] : '', // Set default value from block configuration.
+      '#options' => $options, // Provide vocabulary options.
+      '#required' => TRUE, // Make this field required.
     ];
 
     return $form;
@@ -65,6 +74,7 @@ class CustomTaxonomyBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
+    // Store the submitted vocabulary id in block configuration.
     $this->configuration['vid'] = $form_state->getValue('vid');
   }
 
